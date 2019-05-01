6a4702f7cd84fe56fe602f04bf609fd956793e45
siberry <siberry226@gmail.com>
2019-05-01 17:34:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' );
+  }
+
+  static titleize(string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = []
+
+    let wordArray = string.split(" ")
+
+    for (let i = 0; i < wordArray.length; i++) {
+      if (i == 0) {
+        result.push(this.capitalize(wordArray[i]))
+      }
+      else if (exceptions.includes(wordArray[i])) {
+        result.push(wordArray[i])
+      }
+      else {
+        result.push(this.capitalize(wordArray[i]))
+      }
+    }
+
+    return result.join(" ")
+  }
+
 }
